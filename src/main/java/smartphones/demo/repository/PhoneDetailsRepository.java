fd921a4d7089604222a3915b765c2b50d4d0f780
Michał Tomala <michaltomala63@gmail.com>
2019-04-28 14:03:48 +0200

    add PhoneDetailsRepo

---
+++ src/main/java/smartphones/demo/repository/PhoneDetailsRepository.java
@@ -0,0 +1,9 @@
+package smartphones.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import smartphones.demo.entity.PhoneDetails;
+
+
+public interface PhoneDetailsRepository extends JpaRepository<PhoneDetails, Long> {
+
+}
