40245dd8d4fc6295f731d83018aa239f4fad5134
Michał Tomala <michaltomala63@gmail.com>
2019-06-05 01:55:13 +0200

    add links to signle brand dashboard.js

--- src/main/webapp/js/dashboard.js
+++ src/main/webapp/js/dashboard.js
@@ -44,7 +44,7 @@ function addBrandRecordToList(tbody, brand) {
 
     let name = document.createElement("td");
     let nameLink = document.createElement("a");
-    nameLink.href = "#";
+    nameLink.href = "/admin/brand/single/"+brand.name;
     nameLink.innerHTML = brand.name;
     name.appendChild(nameLink);
 
@@ -120,7 +120,7 @@ function addPhoneRecordToList(tbody,phone) {
 
     let brandName = document.createElement("td");
     let brandLink = document.createElement("a");
-    brandLink.href = "#";
+    brandLink.href = "/admin/brand/single/"+phone.brand.name;
     brandLink.innerHTML = phone.brand.name;
     brandName.appendChild(brandLink);
 
